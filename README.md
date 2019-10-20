861822671dadd5c87b560ff4bfccbc8303240874
David Anekstein <aneksteind@gmail.com>
2019-10-20 11:25:15 -0400

    minor cleanup with project info + replaced at cata with a hylo in mutating the population

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
+#gabble
+
+A small genetic algorithms library meant for prototyping various individual representations and genetic operators.
+
+
+## Building
 `stack install`
 
+## Running
 `stack exec gabble-exe`
--- src/GA.hs
+++ src/GA.hs
@@ -20,6 +20,7 @@ import Control.Monad.RWS.Lazy (RWS, rws, ask, tell, MonadReader, MonadWriter)
 import Data.Functor.Foldable (Fix (..), ListF (..), cata, hylo, embed)
 import Data.Functor.Foldable.Exotic (cataM, anaM)
 import Data.Fix (hyloM)
+import Data.List (sort)
 import Data.Bits ((.&.))
 import qualified Data.Text as T
 import qualified Data.Heap as Heap
@@ -73,11 +74,11 @@ mutateBool p x = do
     indp <- randomD
     if indp < p then return $ not x else return x
 
+toList :: AlgebraM (GAContext a) (ListF a) [a]
+toList = return . embed
+
 makePopulation :: Int -> GAContext a [a]
 makePopulation s = hyloM toList randomInd s where
-    toList :: AlgebraM (GAContext a) (ListF a) [a]
-    toList = return . embed
-
     randomInd :: CoAlgebraM (GAContext a) (ListF a) Int
     randomInd 0 = return Nil
     randomInd n = do
@@ -86,13 +87,14 @@ makePopulation s = hyloM toList randomInd s where
         return $ Cons ind (n-1)
 
 -- mutates individuals in the population with probability `p`
-mutatePop :: Double -> Fix (ListF a) -> GAContext a (Fix (ListF a))
-mutatePop p pop = do cataM abb pop where
-    abb Nil = return nil
-    abb (Cons a mutated) = do
+mutatePop :: Double -> Fix (ListF a) -> GAContext a [a]
+mutatePop p pop = hyloM toList abb pop where
+    abb :: CoAlgebraM (GAContext a) (ListF a) (Fix (ListF a))
+    abb (Fix Nil) = return Nil
+    abb (Fix (Cons a mutated)) = do
         cfg <- ask
         m <- (mutate cfg) p a
-        return $ cons m mutated      
+        return $ Cons m mutated      
 
 -- repeatedly selects two new parents from `parents` from which `n` total children are produced
 reproduceFrom :: (Fix (ListF a)) -> Int -> GAContext a (Fix (ListF a))
@@ -135,22 +137,20 @@ insertHeap hof = cata insert where
     insert (Cons a heap) = Heap.insert a heap
 
 -- updates the HOF by removing the worst-fit individuals from the min-heap
-updateHOF :: Ord a => GASnapshot a -> Fix (ListF a) -> GASnapshot a
-updateHOF res pop = res { hof = newHOF } where
-    currentHOF = hof res
-    newHOF = case (Heap.isEmpty currentHOF) of
+updateHOF :: Ord a => GASnapshot a -> [a] -> GASnapshot a
+updateHOF snapshot pop = snapshot { hof = newHOF } where
+    currentHOF = hof snapshot
+    newHOF = case Heap.isEmpty currentHOF of
         -- initialize the HOF with the `hofSize` best individuals
-        True -> insertHeap currentHOF $ takeF (hofSize res) pop
+        True -> Heap.fromAscList . take (hofSize snapshot) . sort $ pop
         -- update the HOF with the best performers seen thus far
-        False -> Heap.drop (lengthF pop) $ insertHeap currentHOF pop
+        False -> Heap.drop (length pop) $ foldr (Heap.insert) currentHOF pop
 
 step :: Ord a => GASnapshot a -> GAContext a (GASnapshot a)
 step snapshot = do
     cfg <- ask
-    let pop = lastGeneration snapshot
-    let score = fitness cfg
     -- select parents and create the next generation from them
-    selectedParents <- select $ cata Fix pop
+    selectedParents <- select $ cata Fix (lastGeneration snapshot)
     -- use the set of parents to create a new generation
     crossed <- cross selectedParents
     -- mutate the next generation
@@ -158,9 +158,9 @@ step snapshot = do
     -- update the HOF
     let newCtx = updateHOF snapshot mutated
     -- show intermediate results
-    let best = (T.pack . show . reverse . map score . Heap.toList $ hof newCtx) :: T.Text
+    let best = T.pack . show . map (fitness cfg) . Heap.toList $ hof newCtx :: T.Text
     let msg = T.concat ["best: ", best]
-    tell $ ([msg] :: [T.Text])
+    tell [msg]
     -- return the mutated generation
     return $ newCtx {lastGeneration = cata embed mutated}
 
@@ -174,7 +174,6 @@ runN n f a = do
 
 runGA :: Ord a => GAContext a (GASnapshot a)
 runGA = do
-
     cfg <- ask
     -- initialize the population
     initialPop <- makePopulation (popSize cfg)
